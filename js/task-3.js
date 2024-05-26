6eb6cba3b7457d63c55bf38020dfd09197c8d401
Petya0610 <160774838+Petya0610@users.noreply.github.com>
2024-05-26 15:12:10 +0200

    add

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function checkForSpam(message) {
+  const blacklistedWord1 = "spam";
+  const blacklistedWord2 = "sale";
+  const messageLoverCase = message.toLowerCase();
+  const result = messageLoverCase.includes(blacklistedWord1) || messageLoverCase.includes(blacklistedWord2);
+  return result;
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
